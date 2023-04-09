5c40a4bc4997b546f94f7e54cd4fc444e52b4511
kuku <covernat6949@gmail.com>
2023-04-09 15:23:52 +0900

    feat : 채팅 리스트 페이지 생성

---
+++ pages/chatList.tsx
@@ -0,0 +1,10 @@
+import Template from '@/components/ChatList/Template';
+import GlobalTemplate from '@/components/Common/Global/Template';
+
+export default function chatList() {
+  return (
+    <GlobalTemplate>
+      <Template />
+    </GlobalTemplate>
+  );
+}
