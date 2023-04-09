268aefa48dabdb37f349cee95b29b10cefc42aa7
kuku <covernat6949@gmail.com>
2023-04-09 15:20:15 +0900

    feat : Key 발급 컴포넌트 생성

---
+++ components/Key/KeyButton.tsx
@@ -0,0 +1,11 @@
+import Link from 'next/link';
+
+function KeyButton() {
+  return (
+    <Link className="text-white underline font-medium text-base" href={''}>
+      KEY 발급받는 방법
+    </Link>
+  );
+}
+
+export default KeyButton;
