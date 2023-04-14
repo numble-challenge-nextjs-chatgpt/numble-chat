4d836ae6a8f3fe24a88a944d5308d4095f32ef15
kuku <covernat6949@gmail.com>
2023-04-15 00:12:15 +0900

    style : Headwind 익스텐션으로 인한 CSS 정렬

--- components/Common/Global/Template.tsx
+++ components/Common/Global/Template.tsx
@@ -2,7 +2,7 @@ import { StrictPropsWithChildren } from '../CommonTypes';
 
 function Template({ children }: StrictPropsWithChildren) {
   return (
-    <div className="w-full max-w-md h-screen border-solid border-2 border-gray-400 rounded-unifyR mx-5">
+    <div className="w-full min-w-[28rem] max-w-md h-screen border-solid border-2 border-gray-400 rounded-unifyR mx-5">
       {children}
     </div>
   );
