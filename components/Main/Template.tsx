8059c9a76478f0f94a8bf409963cdb097c18fad8
kuku <covernat6949@gmail.com>
2023-04-15 00:19:11 +0900

    style : Headwind 익스텐션으로 인한 CSS 정렬

--- components/Main/Template.tsx
+++ components/Main/Template.tsx
@@ -1,18 +1,18 @@
 import Button from '../Common/Button/Button';
 import Input from '../Common/Input/Input';
 import MainLogo from '../Common/Logo/MainLogo';
-import KeyButton from '../Key/KeyButton';
+import KeyButton from '../Common/Key/KeyButton';
 
 function Template() {
   return (
-    <main className="flex flex-col h-full w-full p-7 items-center justify-between">
+    <main className="flex flex-col items-center justify-between w-full h-full p-7">
       <section className="w-full h-full flex-[2] flex justify-center">
         <MainLogo />
       </section>
       <section className="w-full flex flex-col flex-[2] items-center first-of-type:self-start ">
         <Input title="API KEY" />
       </section>
-      <section className="w-full h-full flex-1 flex flex-col items-center justify-end">
+      <section className="flex flex-col items-center justify-end flex-1 w-full h-full">
         <Button size="L" color="B">
           Login
         </Button>
